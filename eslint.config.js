7053e52c33780162e541b2ae6825ca6c7e93f3db
K-Roon <66551757+K-Roon@users.noreply.github.com>
2025-07-09 20:11:19 +0900

    기본 Base 작업 진행중

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,19 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  {
-    ignores: ['dist/**'],
-  },
-  {
-    rules: {
-      'quotes': ['error', 'single'],
-      'indent': ['error', 2, { 'SwitchCase': 0 }],
-      'linebreak-style': ['error', 'unix'],
-      'semi': ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'dot-notation': 'error',
-      'eqeqeq': ['error', 'smart'],
-      'curly': ['error', 'all'],
-      'brace-style': ['error'],
-      'prefer-arrow-callback': 'warn',
-      'max-len': ['warn', 160],
-      'object-curly-spacing': ['error', 'always'],
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': ['error', { 'classes': false, 'enums': false }],
-      '@typescript-eslint/no-unused-vars': ['error', { 'caughtErrors': 'none' }],
-    },
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-    },
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);

b5f29d38bb985324dde34435c18eb7f84cce1d8d
K-Roon <66551757+K-Roon@users.noreply.github.com>
2025-07-10 12:26:37 +0900

    Update author and README title

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'HejhomeIR';
 /**
  * npm package 이름 (package.json name 필드와 동일)
  */
-export const PLUGIN_NAME = '@K-Roon/homebridge-hejhome-IR';
+export const PLUGIN_NAME = '@k-roon/homebridge-hejhome-ir';
